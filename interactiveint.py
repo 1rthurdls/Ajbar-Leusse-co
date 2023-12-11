7edc11187aab19b74db5fd860b353d214ebc3794
1rthurdls <150895465+1rthurdls@users.noreply.github.com>
2023-12-11 12:33:32 +0100

    Add files via upload

--- interactiveint.py
+++ interactiveint.py
@@ -28,18 +28,30 @@ reall_data=np.array(dataa_used,dtype=[('date', 'U10'), ('citi', 'U20'), ('col1',
 
 
 if  len(sys.argv) < 2:
-    print("this is a message for the user, in order for you toaccess the informaation you need, you need follow this structure")
-    print("python 'name of the file' 'city your are interseted in' 'number of data you are searching'")
+    print("this is a message for the user, in order for you to access the informations you need, you have to follow this structure")
+
+    print("'python' 'name of the file' 'city your are interseted in' 'number of data you are searching'")
+
     print("we divided the data into mutiple categories that have a number ")
+
     print("if you are searching for the hottest and coldest day of a city, type 1")
+
     print("if you are searching for the overall hottest temperature's day of a city informations, type 2")
+
     print("if you are searching for the overall coldest temperature's day of a city informations, type 3")
+
     print("if you are searching for knowing if a day or a city was hot,moderate or cold, type 4")
+
     print("if you are searching for the average temperature of a city type 5")
+
     print("if you are searching for the total precipitation of a city type 6")
+
     print("if you are searching for maximum and minimum wind speed of a city type 7")
+
     print("if you are searching for a graph representing the temperature over time of a city type 8")
+
     print("if you are searching for a bar chart that compares the average temperature of the different cities type All as if it was a city then type 9.")
+
     print("if you are searching for a chart that illustrate the relationship between temperature, precipitation, and climate change factors type All as if it was a city then type 10.")
 
     sys.exit()
@@ -134,7 +146,7 @@ elif  len(sys.argv) ==3:
              sys.exit()
     
     else :
-            print("ERROR,errortype city not in database")
+            print("ERROR,errortype the number does not corespond to a certain data please enter a number in range 1 to 10")
             sys.exit()
 
 
